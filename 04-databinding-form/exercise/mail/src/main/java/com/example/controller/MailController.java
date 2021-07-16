35012596f38416207fd4407ef2674323533ffb05
AiYuki <khinhvu2707@gmail.com>
2021-07-16 15:51:58 +0700

    commit bài tập bài 4

--- 04-databinding-form/exercise/mail/src/main/java/com/example/controller/MailController.java
+++ 04-databinding-form/exercise/mail/src/main/java/com/example/controller/MailController.java
@@ -2,7 +2,6 @@ package com.example.controller;
 
 import com.example.model.Email;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
